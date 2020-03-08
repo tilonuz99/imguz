dc731a5f2b2ab641ecdf46009c01e2c0ba3da608
tilonuz99 <59096186+tilonuz99@users.noreply.github.com>
2020-03-08 18:48:04 +0500

    Update Settings.php

--- TilonLib/Settings.php
+++ TilonLib/Settings.php
@@ -3,7 +3,7 @@ namespace Tilon;
 
 class Settings
 {
-    const token = 'TokenJoyi';
+    const token = '1001821005:AAGTgpmk8i2zSjNSJdtDzwz94unHL_4gXr0';
     const admin = 630751054;
     const botlink = 'tilonbot';
     const botId = 1001821005;
